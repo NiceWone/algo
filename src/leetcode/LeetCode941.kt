92c3c4875b1bdf4561018c2ba5e8f9491e3f81f5
Igor Prokaev <igor.prokaev@rosbank.ru>
2024-06-08 15:19:29 +0300

    LeetCode941.kt

--- src/leetcode/LeetCode941.kt
+++ src/leetcode/LeetCode941.kt
@@ -4,38 +4,28 @@ fun main() {
     val leetCode = LeetCode941()
 
 //    println(leetCode.validMountainArray(intArrayOf(2, 1)))
+//    println(leetCode.validMountainArray(intArrayOf(1, 3, 2)))
 //    println(leetCode.validMountainArray(intArrayOf(3, 5, 5)))
 //    println(leetCode.validMountainArray(intArrayOf(0, 3, 2, 1)))
-    println(leetCode.validMountainArray(intArrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)))
+//    println(leetCode.validMountainArray(intArrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)))
+    println(leetCode.validMountainArray(intArrayOf(9, 8, 7, 6, 5, 4, 3, 2, 1, 0)))
 }
 
 class LeetCode941 {
     fun validMountainArray(arr: IntArray): Boolean {
-        if (arr.size < 3) {
+        if (arr.size < 3 || arr[0] >= arr[1] || arr[arr.lastIndex - 1] <= arr[arr.lastIndex]) {
             return false
         }
-        // find max & index
-        var indexMax = 0
-        var valueMax = arr[0]
-        for (i in 0..arr.lastIndex) {
-            if (valueMax < arr[i]) {
-                indexMax = i
-                valueMax = arr[i]
-            }
-        }
-        if (indexMax == arr.lastIndex || indexMax == 0) {
-            return false
-        }
-        // max to start
-        for (i in 0..<indexMax - 1) {
-            if (arr[i + 1] <= arr[i]) {
-                return false
-            }
-        }
-        // max to end
-        for (i in indexMax..<arr.size - 1) {
-            if (arr[i + 1] >= arr[i]) {
-                return false
+        var isTopFound = false
+        for (i in 0..<arr.lastIndex) {
+            if (!isTopFound) {
+                if (arr[i] >= arr[i + 1]) {
+                    isTopFound = true
+                }
+            } else {
+                if (arr[i] <= arr[i + 1]) {
+                    return false
+                }
             }
         }
         return true
